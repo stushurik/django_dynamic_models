17061b30f6df2234c621e64ff3a36c6f80b48a65
stushurik <stu.shurik@gmail.com>
2013-10-02 18:53:53 +0300

    base html template base urls config fix some settings bugs

---
+++ django_dynamic_models/dynamic_models/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls.defaults import patterns
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^django_dynamic_models/', include('django_dynamic_models.foo.urls')),
+)
--- django_dynamic_models/settings/settings.py
+++ django_dynamic_models/settings/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(os.path.dirname(DJANGO_PROJECT_ROOT),'db.sqlite3'),# Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -98,9 +98,9 @@ INSTALLED_APPS = (
 
     'south',
 
-    'dynamic_models'
+    'dynamic_models',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
